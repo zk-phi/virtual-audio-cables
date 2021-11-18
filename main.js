39a482a5ea7cbf5758ebe33f96837b4c6b475dfd
zk-phi <zk-phi@users.noreply.github.com>
2021-11-18 14:18:26 +0900

    Text

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
 app.on('ready', e => {
-  const mainWindow = new BrowserWindow({ width: 182, height: 428 });
+  const mainWindow = new BrowserWindow({ width: 190, height: 432 });
   mainWindow.setAlwaysOnTop(true);
   mainWindow.loadFile("./index.html");
   mainWindow.setTitle("外音取り込み君");
